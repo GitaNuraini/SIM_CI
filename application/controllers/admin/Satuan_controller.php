1fef2f7ab04e2854561d626cc4dbdf71eac69d2a
Gita Nuraini <nurainigita03@gmail.com>
2019-06-15 16:08:16 +0700

    satuan

--- application/controllers/admin/Satuan_controller.php
+++ application/controllers/admin/Satuan_controller.php
@@ -2,19 +2,19 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Products extends CI_Controller
+class Satuan_controller extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("product_model");
+        $this->load->model("satuan_model");
         $this->load->library('form_validation');
     }
 
     public function index()
     {
-        $data["data_barang"] = $this->product_model->getAll();
-        $this->load->view("admin/product/list", $data);
+        $data["data_satuan"] = $this->satuan_model->getAll();
+        $this->load->view("admin/satuan/list", $data);
     }
 
     public function add()
@@ -28,12 +28,12 @@ class Products extends CI_Controller
             $this->session->set_flashdata('success', 'Berhasil disimpan');
         }
 
-        $this->load->view("admin/product/new_form");
+        $this->load->view("admin/satuan/new_form");
     }
 
     public function edit($id = null)
     {
-        if (!isset($id)) redirect('admin/products');
+        if (!isset($id)) redirect('admin/satuan_controller');
        
         $product = $this->product_model;
         $validation = $this->form_validation;
@@ -47,15 +47,15 @@ class Products extends CI_Controller
         $data["product"] = $product->getById($id);
         if (!$data["product"]) show_404();
         
-        $this->load->view("admin/product/edit_form", $data);
+        $this->load->view("admin/satuan/edit_form", $data);
     }
 
     public function delete($id=null)
     {
         if (!isset($id)) show_404();
         
-        if ($this->product_model->delete($id)) {
-            redirect(site_url('admin/products'));
+        if ($this->satuan_model->delete($id)) {
+            redirect(site_url('admin/satuan_controller'));
         }
     }
 }
