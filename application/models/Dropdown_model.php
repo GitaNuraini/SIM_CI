2cd9f43037644e28d1b6466e3ae56bd37f3ac5eb
Gita Nuraini <nurainigita03@gmail.com>
2019-06-16 15:29:35 +0700

    Update Dropdown_model.php

--- application/models/Dropdown_model.php
+++ application/models/Dropdown_model.php
@@ -4,7 +4,7 @@ class Dropdown_model extends CI_Model {
 function get_kategori() {
  $this->db->select('*');
  $this->db->from('data_kategori');
- $this->db->order_by('id_kategori', 'desc');
+ 
  $query = $this->db->get();
  return $query->result();
 }
@@ -12,7 +12,6 @@ function get_kategori() {
  function get_satuan() {
  $this->db->select('*');
  $this->db->from('data_satuan');
- $this->db->order_by('id_satuan', 'desc');
  $query = $this->db->get();
  return $query->result();
 }
